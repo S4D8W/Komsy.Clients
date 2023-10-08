a4b9b8ea38546bffee07a2c77dd60902f6d6f774
Igor Wieczorek <igorw645@gmail.com>
2023-10-08 09:40:35 +0200

    init project

--- Komsy.web/Komsy.web/Program.cs
+++ Komsy.web/Komsy.web/Program.cs
@@ -1,4 +1,4 @@
-using Komsy.web;
+﻿using Komsy.web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -6,6 +6,7 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
